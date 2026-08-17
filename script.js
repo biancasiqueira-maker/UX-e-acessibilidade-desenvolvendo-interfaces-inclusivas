3fbe85cecaabb7bca825fa8967f6a80faf3aac21
biancasiqueira-maker <bianca.siqueira@escola.pr.gov.br>
2026-08-17 17:21:29 -0300

    updait

---
+++ script.js
@@ -0,0 +1,7 @@
+let bntAjuda = document.querySelector(".botao-ajuda-inicio");
+let bntFechar = document.querySelector(".botao-fechar-inicio");
+let modal = document.querySelector(".botao-ajuda-inicio");
+
+bntAjuda.addEventListener("click", abreModal);
+bntFechar.addEventListener("click", fechaModal);
+
